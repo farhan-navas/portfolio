d8a4ea485b928516f82fc392b6f7967a671c0d89
farhan-navas <farhanmnavas@gmail.com>
2025-08-03 21:53:34 +0800

    init theme provider for dark and light mode switching

--- src/app/_components/nav-bar.tsx
+++ src/app/_components/nav-bar.tsx
@@ -5,7 +5,7 @@ import { Dialog, DialogPanel } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { ModeToggle } from "./mode-toggle";
 
 const navigation = [
   { name: "Home", href: "/" },
@@ -17,72 +17,77 @@ const navigation = [
 
 export default function NavBar() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-  const pathName = usePathname();
 
   return (
     <header className="absolute inset-x-0 top-0 z-50">
-      <div className="mx-auto max-w-7xl">
-        <div className="px-6 pt-6 lg:max-w-2xl lg:pl-8 lg:pr-0">
-          <nav
-            className="flex items-center justify-between lg:justify-start"
-            aria-label="Global"
-          >
-            <button
-              type="button"
-              className="-m-2.5 rounded-md p-2.5 text-gray-700 lg:hidden"
-              onClick={() => setMobileMenuOpen(true)}
-            >
-              <span className="sr-only">Open main menu</span>
-              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-            </button>
-            <div className="hidden lg:ml-12 lg:flex lg:gap-x-14">
-              {navigation.map((item) => (
-                <Link
-                  key={item.name}
-                  href={item.href}
-                  className={
-                    pathName === item.href
-                      ? "text-sm font-semibold leading-6 text-indigo-600 hover:text-indigo-600"
-                      : "text-sm font-semibold leading-6 text-gray-900 hover:text-indigo-600"
-                  }
-                >
-                  {item.name}
-                </Link>
-              ))}
-            </div>
-          </nav>
+      <nav
+        aria-label="Global"
+        className="flex items-center justify-between p-6 lg:px-8"
+      >
+        <div className="flex lg:flex-1">
+          <a href="#" className="-m-1.5 p-1.5">
+            <span className="sr-only">frhnnvs</span>
+            <img alt="" src="frhnnvs-logo.png" className="h-8 w-auto" />
+          </a>
         </div>
-      </div>
+        <div className="flex lg:hidden">
+          <button
+            type="button"
+            onClick={() => setMobileMenuOpen(true)}
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+          >
+            <span className="sr-only">frhnnvs</span>
+            <Bars3Icon aria-hidden="true" className="size-6" />
+          </button>
+        </div>
+        <div className="hidden lg:flex lg:gap-x-12">
+          {navigation.map((item) => (
+            <a
+              key={item.name}
+              href={item.href}
+              className="text-sm/6 font-semibold text-gray-900"
+            >
+              {item.name}
+            </a>
+          ))}
+          <ModeToggle />
+        </div>
+      </nav>
       <Dialog
-        as="div"
-        className="lg:hidden"
         open={mobileMenuOpen}
         onClose={setMobileMenuOpen}
+        className="lg:hidden"
       >
         <div className="fixed inset-0 z-50" />
-        <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
+            <a href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">frhnnvs</span>
+              <img alt="" src="frhnnvs-logo.png" className="h-8 w-auto" />
+            </a>
             <button
               type="button"
-              className="-m-2.5 rounded-md p-2.5 text-gray-700"
               onClick={() => setMobileMenuOpen(false)}
+              className="-m-2.5 rounded-md p-2.5 text-gray-700"
             >
               <span className="sr-only">Close menu</span>
-              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+              <XMarkIcon aria-hidden="true" className="size-6" />
             </button>
           </div>
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
+                <ModeToggle />
+              </div>
+              <div className="space-y-2 py-6">
                 {navigation.map((item) => (
-                  <Link
+                  <a
                     key={item.name}
                     href={item.href}
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:text-indigo-600 hover:bg-gray-50"
-                    onClick={() => setMobileMenuOpen(false)}
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
                   >
                     {item.name}
-                  </Link>
+                  </a>
                 ))}
               </div>
             </div>
